f11f7838c232a62992dd1e21aa94ea18d98c0105
immaturehearts <1120841716@qq.com>
2022-01-14 13:07:03 +0800

    启动界面
    新增启动界面

--- app/src/main/java/com/example/epcmsystem/MainActivity.java
+++ app/src/main/java/com/example/epcmsystem/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.epcmsystem;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().hide();// 隐藏ActionBar
+        }
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+
+            }
+        };
+        timer.schedule(task,3000);
     }
 }
