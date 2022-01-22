c2e4b589595afb09e6c79e9abf29eb08f634b66a
immaturehearts <1120841716@qq.com>
2022-01-22 15:26:19 +0800

    修复bug

--- app/src/main/java/com/example/epcmsystem/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/epcmsystem/ui/home/HomeFragment.java
@@ -1,15 +1,18 @@
 package com.example.epcmsystem.ui.home;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -22,7 +25,6 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
-    private LinearLayout layout;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -31,7 +33,6 @@ public class HomeFragment extends Fragment {
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
-        layout = (LinearLayout) root.findViewById(R.id.home_ll);
 
 //        NavigationView myselfView = (NavigationView) root.findViewById(R.id.home_nv);
 //        myselfView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -42,6 +43,11 @@ public class HomeFragment extends Fragment {
 //        });
 
         return root;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
     }
 
     @Override
